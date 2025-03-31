ad098bbdb8aaafdf176ed0da284b875a983dfc0e
grifsc <griffin@sin-chan.com>
2025-03-31 15:31:58 -0700

    added header to indicate page

---
+++ TeamFormation/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+    
+<h1>MYVC Montreal</h1>
+
+<nav class = "navbar">
+    <ul>
+        <li><a href ="../home.php">Home</a></li>
+        <li><a href ="../Location/index.php">Club Location</a></li>
+        <li><a href ="../Personnel/index.php">Personnel</a></li>
+        <li><a href ="../FamilyMember/index.php">Family Member</a></li>
+        <li><a href ="../ClubMember/index.php">Club Member</a></li>
+        <li><a href ="../TeamFormation/index.php">Team Formation</a></li>
+    </ul>
+</nav>
+<main>
+    <h2>Team Formation</h2>
+</main>
+
+</body>
+</html>
